719cd1f94784eb5302f516cc966514cdb2883fe5
Abhijeet-Khavaskar <abhi.625khavaskar@gmail.com>
2024-03-26 21:15:01 +0530

    hero section upload

--- qtify/src/Components/Navbar/Navbar.jsx
+++ qtify/src/Components/Navbar/Navbar.jsx
@@ -3,13 +3,13 @@ import Button from "../Button/Button";
 import Search from "../Search/Search";
 import styles from './Navbar.module.css'
 // import LogoImage from "../../assets/Logo.png"
-import path from "../../assets/Logo.png"
+// import path from "../../assets/Logo.png"
 
 export default function Navbar(){
     return(
         <>
             <nav className={styles.navbar}>
-                <div className={styles.logoDiv}><img  src={path} alt="logo" width={67}/></div>
+                <div className={styles.logoDiv}><img  src="Logo.png" alt="logo" width={67}/></div>
                  <Search search={"Search a song of your choice"}/>
                 <Button children="Give Feedback"/>
             </nav>
