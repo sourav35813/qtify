2ef9b749790bcc160faf55b6d23adf0f2ceb6b1c
Abhishek Patil <abishkpatil@gmail.com>
2024-04-20 22:50:44 +0530

    added Theme, give feedback button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home"
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Define custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      900: '#016A00',
+      800: '#028904',
+      700: '#009B12',
+      600: '#00AC20',
+      500: '#01BD2B',
+      400: '#35C94B',
+      300: '#65D36E',
+      200: '#C1ECC2',
+      100: '#C1ECC2',
+      50: '#E6F7E5'
+    },
+    customBlack: {
+      main: '#121212',
+    },
+    customWhite: {
+      main: '#FFFFFF',
+    }
+  },
+});
 
 function App() {
   return <>
-  <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   </>
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import { ClassNames } from "@emotion/react";
 import Logo from "./Logo";
 import styles from "./styles.module.css"
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
 
 const Navbar = () => {
+    const theme = useTheme();
     return (
         <div className={styles.container}>
             <Logo />
+            <Button color="customBlack" variant="contained" sx={{ color: theme.palette.primary[400] }}>Give Feedback</Button>
         </div>
     )
 }
