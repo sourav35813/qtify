09f18c5cd4ba2a3e7112ce7bf2be2590d0c9e81f
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-04-20 22:29:06 +0530

    testd

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & TS
+          Learn React & TSg
         </a>
       </header>
     </div>
