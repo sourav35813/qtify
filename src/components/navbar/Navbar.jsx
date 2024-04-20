dc89838b1d09a6b4e0230e51947b611b8a9ccb0d
Abhishek Patil <abishkpatil@gmail.com>
2024-04-21 02:01:17 +0530

    c4: search bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,14 +3,21 @@ import Logo from "./Logo";
 import styles from "./styles.module.css"
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
+import Stack from '@mui/system/Stack';
+import { Searchbar } from "./Searchbar";
 
 const Navbar = () => {
     const theme = useTheme();
     return (
-        <div className={styles.container}>
-            <Logo />
-            <Button color="customBlack" variant="contained" sx={{ color: theme.palette.primary[400] }}>Give Feedback</Button>
-        </div>
+        <>
+            <div className={styles.container}>
+                <Stack direction="row" justifyContent="space-between" alignItems="center">
+                    <Logo />
+                    <Searchbar />
+                    <Button color="customBlack" variant="contained" sx={{ color: theme.palette.primary[400], mr: '32px', borderRadius: '8px' }}>Give Feedback</Button>
+                </Stack>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/navbar/Searchbar.jsx
@@ -0,0 +1,26 @@
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/system/Box'
+import { InputBase } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Divider from '@mui/material/Divider';
+
+const Searchbar = () => {
+    const theme = useTheme();
+    return <>
+    {/* sx={{ height: '48px', pl: '15px', mt: '13px', flex: 1, border: '1px solid black', width: '568px', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent:'space-between' }} */}
+        <Box sx={{display:'flex'}}>
+            <InputBase
+                placeholder="Search a album of your choice"
+                sx={{height:'48px', bgcolor:'white', py: '4px',pl: '15px', border: '1px solid black', width: '568px', borderRadius: '8px', borderTopRightRadius:0, borderBottomRightRadius:0}}
+            />
+            <IconButton type="button" aria-label="search" sx={{px:'14px', bgcolor:'white', border:'1px solid black', borderRadius:'8px', borderTopLeftRadius:0, borderBottomLeftRadius:0, borderLeft:0}}>
+                <SearchIcon/>
+            </IconButton>
+        </Box>
+
+    </>
+}
+
+export { Searchbar };
