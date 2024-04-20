2b34471c60fdec806be24948854e4773394695e3
Abhishek Patil <abishkpatil@gmail.com>
2024-04-21 02:43:03 +0530

    added space to logo

--- src/components/navbar/Searchbar.jsx
+++ src/components/navbar/Searchbar.jsx
@@ -12,7 +12,7 @@ const Searchbar = () => {
     {/* sx={{ height: '48px', pl: '15px', mt: '13px', flex: 1, border: '1px solid black', width: '568px', borderRadius: '8px', display: 'flex', alignItems: 'center', justifyContent:'space-between' }} */}
         <Box sx={{display:'flex'}}>
             <InputBase
-                placeholder="Search a album of your choice"
+                placeholder="Search a song of your choice"
                 sx={{height:'48px', bgcolor:'white', py: '4px',pl: '15px', border: '1px solid black', width: '568px', borderRadius: '8px', borderTopRightRadius:0, borderBottomRightRadius:0}}
             />
             <IconButton type="button" aria-label="search" sx={{px:'14px', bgcolor:'white', border:'1px solid black', borderRadius:'8px', borderTopLeftRadius:0, borderBottomLeftRadius:0, borderLeft:0}}>
