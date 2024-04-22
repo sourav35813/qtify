e60626c4dbe0e37862b4730a48c363409648e903
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-04-22 19:58:38 +0530

    dkd

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -8,15 +8,15 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p> */}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & TSg
-        </a>
+          Learn React & TSgj
+        </a> */}
       </header>
     </div>
   );
