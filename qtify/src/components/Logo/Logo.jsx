a4515bdfd479bdad66a338903fab3f1f505383ac
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-04-24 23:09:49 +0530

    test

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoImage from '../../assets/Logo.png';
+
+export default function Logo() {
+    return (
+        <div id="logodiv">
+    <img src={LogoImage} alt="logo" width={47} />
+    </div>
+)
+}
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import Logo from '../Logo/Logo';
+// // import Search from '../Search/Search';
+import './navbar.modules.css';
+
+function Navbar({ searchData }) {
+    return (
+        <nav>
+        {/* // <nav className={styles.navbar}> */}
+        {/* <Link to='/'> */}
+            <Logo />
+        {/* </Link> */}
+        </nav>
+    )
+}
+
+export default Navbar;
+
+
+
+// import React from "react";
+// // import Button from "../Button/Button";
+// // import Search from "../Search/Search";
+// import styles from './navbar.modules.css'
+// // import LogoImage from "../../assests/logo.png"
+
+// export default function Navbar(){
+//     return(
+//         <div>
+//             <nav className={styles.navbar}>
+//                 <divs className={styles.logoDiv} >
+//                     {/* <img src={LogoImage} alt="logo" width={67}/> */}
+//                     <Logo />
+//                     </div>
+//                 {/* <Search search={"Search a song of your choice"}/> */}
+//                 {/* <Button children="Give Feedback"/> */}
+//             </nav>
+//         </div>
+//     )
+// }
