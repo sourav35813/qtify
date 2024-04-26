62f84d43801ab84b46eeeaefc5fb65bae5f790e7
Abhishek Patil <abishkpatil@gmail.com>
2024-04-26 13:28:26 +0530

    feat: add tooltip for albumCard to display songs count

--- src/components/album/AlbumCard.jsx
+++ src/components/album/AlbumCard.jsx
@@ -2,44 +2,94 @@ import { Box } from "@mui/material";
 import { Chip } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import { DateRange } from "@mui/icons-material";
 
-const AlbumCard = ({ data, isSongsSection=false }) => {
+const AlbumCard = ({ data, isSongsSection = false }) => {
     const theme = useTheme();
     let count;
-    if(!isSongsSection){
+    let title;
+    if (!isSongsSection) {
         count = data.follows + " Follows"
-    }else{
+        title = data.songs.length + " songs";
+    } else {
         count = data.likes + " Likes"
     }
     return <>
-        <Box
-            sx={{
-                width: '180px',
-                height: '240px',
-                backgroundColor: 'white',
-                borderRadius: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                overflow: 'hidden',
-            }}
-        >
-            <img
-                src={data.image}
-                alt="Your Image"
-                style={{
-                    width: '100%',
-                    height: '84%',
-                    objectFit: 'cover',
-                }}
-            />
-            <Box display="flex" alignItems="center" sx={{ backgroundColor: "white", height: '16%' }}>
-                <Chip size="small" label={count} sx={{ color: 'white', ml: '10px', backgroundColor: theme.palette.customBlack.main, fontSize: '11px' }} />
-            </Box>
-        </Box>
-        <Typography variant="body2" sx={{ pt: '5px', pb:'10px' }}>
-            {data.title}
-        </Typography>
+        {
+            isSongsSection ?
+                (
+                    <>
+                        <Box
+                            sx={{
+                                width: '180px',
+                                height: '240px',
+                                backgroundColor: 'white',
+                                borderRadius: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <img
+                                src={data.image}
+                                alt="Your Image"
+                                style={{
+                                    width: '100%',
+                                    height: '84%',
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <Box display="flex" alignItems="center" sx={{ backgroundColor: "white", height: '16%' }}>
+                                <Chip size="small" label={count} sx={{ color: 'white', ml: '10px', backgroundColor: theme.palette.customBlack.main, fontSize: '11px' }} />
+                            </Box>
+                        </Box>
+                        <Typography variant="body2" sx={{ pt: '5px', pb: '10px' }}>
+                            {data.title}
+                        </Typography>
+                    </>
+                )
+                :
+                (
+                    <Tooltip
+                        title={title}
+                        placement="top"
+                        arrow
+                        display="flex"
+                        justifyContent="center"
+                    >
+                        <Box
+                            sx={{
+                                width: '180px',
+                                height: '240px',
+                                backgroundColor: 'white',
+                                borderRadius: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'space-between',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            <img
+                                src={data.image}
+                                alt="Your Image"
+                                style={{
+                                    width: '100%',
+                                    height: '84%',
+                                    objectFit: 'cover',
+                                }}
+                            />
+                            <Box display="flex" alignItems="center" sx={{ backgroundColor: "white", height: '16%' }}>
+                                <Chip size="small" label={count} sx={{ color: 'white', ml: '10px', backgroundColor: theme.palette.customBlack.main, fontSize: '11px' }} />
+                            </Box>
+                        </Box>
+                        <Typography variant="body2" sx={{ pt: '5px', pb: '10px' }}>
+                            {data.title}
+                        </Typography>
+                    </Tooltip>
+                )
+        }
     </>
 }
 
