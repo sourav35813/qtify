06d91fd7e790aaef5fc7960efed6a556d2549687
Abhishek Patil <abishkpatil@gmail.com>
2024-04-28 16:52:01 +0530

    feat: add Give Feedback button & form

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,8 +8,7 @@ import { Searchbar } from "./Searchbar";
 import RateReviewOutlinedIcon from '@mui/icons-material/RateReviewOutlined';
 import { useMediaQuery, Hidden } from '@mui/material';
 import Box from "@mui/material/Box";
-
-
+import { FeedBack } from "./FeedBack";
 const Navbar = () => {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down('sm'));
@@ -19,24 +18,12 @@ const Navbar = () => {
             <div className={styles.container}>
                 <Hidden mdUp>
                         <Logo />
-                        <Button
-                            color="customBlack"
-                            variant="contained"
-                            sx={{ color: theme.palette.customWhite.main, mr: '32px', borderRadius: '8px', height: '30px', width: '2px' }}
-                        >
-                            <RateReviewOutlinedIcon sx={{ height: '20px', padding: '0px' }} />
-                        </Button>
+                        <FeedBack />
                 </Hidden>
                 <Hidden mdDown>
                     <Logo />
                     <Searchbar />
-                    <Button
-                        color="customBlack"
-                        variant="contained"
-                        sx={{ color: theme.palette.primary[400], mr: '32px', borderRadius: '8px' }}
-                    >
-                        Give Feedback
-                    </Button>
+                    <FeedBack />
                 </Hidden>
             </div>
         </>
