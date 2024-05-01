ac5b88452ba313e31e53e5fbc677a193eb6dcf57
Abhishek Patil <abishkpatil@gmail.com>
2024-05-01 12:29:40 +0530

    fix: searchbar border radius set to 0 when on small screen

--- src/components/navbar/Searchbar.jsx
+++ src/components/navbar/Searchbar.jsx
@@ -14,9 +14,9 @@ const Searchbar = () => {
         <Box sx={{display:'flex', minWidth: isSmallScreen ? '100vw' : '568px', maxWidth: isSmallScreen ? '100vw' : '568px'}}>
             <InputBase
                 placeholder="Search a song of your choice"
-                sx={{width:'100%', height:'48px', bgcolor:'white', py: '4px',pl: '15px', border: '1px solid black' , borderRadius: '8px', borderTopRightRadius:0, borderBottomRightRadius:0}}
+                sx={{width:'100%', height:'48px', bgcolor:'white', py: '4px',pl: '15px', border: '1px solid black' , borderRadius: isSmallScreen ? 0 : '8px', borderTopRightRadius:0, borderBottomRightRadius:0}}
             />
-            <IconButton type="button" aria-label="search" sx={{px:'14px', bgcolor:'white', border:'1px solid black', borderRadius:'8px', borderTopLeftRadius:0, borderBottomLeftRadius:0, borderLeft:0}}>
+            <IconButton type="button" aria-label="search" sx={{px:'14px', bgcolor:'white', border:'1px solid black', borderRadius: isSmallScreen ? 0 : '8px', borderTopLeftRadius:0, borderBottomLeftRadius:0, borderLeft:0}}>
                 <SearchIcon/>
             </IconButton>
         </Box>
