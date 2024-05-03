27b4127b9c6ef31ac45c818680a6b6ded8640656
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-05-04 02:11:12 +0530

    gf

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -3,7 +3,6 @@ import styles from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    // <div id="logodiv">
     <div className={styles.logodiv}>
       <img src={LogoImage} alt="logo" />
     </div>
