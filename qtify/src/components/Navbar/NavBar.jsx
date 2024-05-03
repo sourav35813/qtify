2561ccf0a618b684ea919e37cc6c9b9bc48b1b95
Ayush Mishra <ayushmishra9799@gmail.com>
2024-05-04 00:42:56 +0530

    Update and rename Navbar.jsx to NavBar.jsx

--- qtify/src/components/Navbar/NavBar.jsx
+++ qtify/src/components/Navbar/NavBar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Button from "../Button/Button";
 import Search from "../Search/Search";
-import styles from "./NavBar.module.css";
+import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
-// import LogoImage from "../../assets/Logo.png";
 
 export default function Navbar() {
   return (
