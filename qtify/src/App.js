65370f993db62b437e7a611121bb69c81d2c9e2f
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-05-04 15:11:27 +0530

    ghj

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,115 @@
-import React from "react";
-import NavBar from "./components/Navbar/Navbar";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import styles from "./App.module.css";
+import FilterSection from "./components/FilterSec/FilterSection";
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from "./api/api";
+import Section from "./components/Section/Section";
 
-export default function App() {
+function App() {
+  const [topAlbumSongs, setTopAlbumSongs] = useState([]);
+  const [newAlbumSongs, setNewAlbumSongs] = useState([]);
+
+  const [filteredDataValues, setFilteredDataValues] = useState([""]);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const generateSongsData = (value) => {
+    let songData = newAlbumSongs[0].songs;
+    let key;
+    if (value === 0) {
+      setFilteredDataValues(songData);
+      return;
+    } else if (value === 1) {
+      key = "rock";
+    } else if (value === 2) {
+      key = "pop";
+    } else if (value === 3) {
+      key = "jazz";
+    } else if (value === 4) {
+      key = "blues";
+    }
+    const data = songData.filter((item) => {
+      return item.genre.key === key;
+    });
+    setFilteredDataValues(data);
+  };
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    generateSongsData(newValue);
+  };
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const filteredData = (val) => {
+    generateSongsData(val);
+    // console.log(val + ' filteredData is called from app.js')
+  };
+
+  const generateTopAlbumSongs = async () => {
+    try {
+      const topAlbumSongs = await fetchTopAlbums();
+      setTopAlbumSongs(topAlbumSongs);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+  const generateNewAlbumSongs = async () => {
+    try {
+      const newAlbumSongs = await fetchNewAlbums();
+      setNewAlbumSongs(newAlbumSongs);
+      // setFilteredDataValues(newAlbumSongs);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  const generateFilterSongs = async () => {
+    try {
+      const newAlbumSongs = await fetchSongs();
+      // setNewAlbumSongs(newAlbumSongs);
+      setFilteredDataValues(newAlbumSongs);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line
+  }, [value]);
+
+  useEffect(() => {
+    generateTopAlbumSongs();
+    generateNewAlbumSongs();
+    generateFilterSongs();
+    // setFilteredDataValues(newAlbumSongs);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <Hero />
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <div className={styles.sectionWrapper}>
+        <Section type="album" title="Top Albums" data={topAlbumSongs} />
+        <Section type="album" title="New Albums" data={newAlbumSongs} />
+        <FilterSection
+          data={newAlbumSongs}
+          type="songFilter"
+          title="Songs"
+          filteredData={filteredData}
+          filteredDataValues={filteredDataValues}
+          value={value}
+          handleChange={handleChange}
+          handleToggle={handleToggle}
+        />
+      </div>
+    </>
   );
 }
+
+export default App;
