ef621acf8d93381e7df2f18c6905410d315430be
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-05-04 10:48:04 +0530

    hh

--- qtify/src/components/Button/Button.jsx
+++ qtify/src/components/Button/Button.jsx
@@ -2,5 +2,5 @@
 import styles from "./Button.module.css";
 
 export default function App() {
-  return <button className={styles.button}>Give FeedBack</button>;
+  return <button className={styles.button}>Give Feedback</button>;
 }
