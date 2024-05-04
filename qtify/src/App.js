ff1d2e618a91c97c3aa0d52871ef34646b4ca3b0
Ayush Mishra (techy boy) <ayushmishra9799@gmail.com>
2024-05-04 14:26:14 +0530

    dd

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <Hero />
       </header>
     </div>
   );
